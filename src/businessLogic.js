37459f946c9a50d8fe6e55384cd3c1cea694b6eb
Paulo dos Santos <paulo@motovalor.com>
2021-03-03 16:40:47 -0300

    refactored graphql query

--- src/businessLogic.js
+++ src/businessLogic.js
@@ -7,9 +7,10 @@ const authenticator = new Authenticator(lxr.instance, lxr.apiToken)
 const graphql = new GraphQLClient(authenticator)
 
 const generateBcMaps = async () => {
-  const query = `
+  const maxHierarchyLevel = 4
+  let query = `
   {
-    allFactSheets(filter: {facetFilters: [{facetKey: "FactSheetTypes", keys: ["BusinessCapability"]}]}, sort: [{key: "level", order: desc}]) {
+    allFactSheets(filter: {facetFilters: [{facetKey: "FactSheetTypes", keys: ["BusinessCapability"]}, {facetKey: "hierarchyLevel", keys: ["1"]}]}, sort: [{key: "level", order: desc}]) {
       edges {
         node {
           id
@@ -17,54 +18,35 @@ const generateBcMaps = async () => {
           name
           description
           level
-          ... on BusinessCapability {
-            parentId: relToParent {
-              edges {
-                node {
-                  factSheet {
-                    id
-                  }
-                }
-              }
-            }
-          }
+          {{children}}
         }
       }
     }
   }
   `.replace(/\s\s+/g, ' ')
-  const businessCapabilities = await graphql.executeGraphQL(query)
-    .then(
-      ({ allFactSheets: { edges } }) => edges
-        .map(({ node }) => {
-          let { description = null, parentId: { edges: [{ node: { factSheet: { id: parentId = null } = {} } = {} } = {}] } } = node
-          delete node.description
-          if (description === null) description = '{}'
-          try {
-            description = JSON.parse(description === null ? '{}' : description)
-          } catch (error) {
-            console.error(error)
-            description = {}
-          }
-          return { ...node, parentId, ...description }
-        })
-    )
-  const businessCapabilityIndex = businessCapabilities
-    .reduce((accumulator, businessCapability) => ({ ...accumulator, [businessCapability.id]: businessCapability }), {})
-  const businessCapabilityMapIndex = Object.values(businessCapabilityIndex)
-    .reduce((accumulator, businessCapability) => {
-      const { parentId = null } = businessCapability
-      delete businessCapability.parentId
-      delete businessCapability.level
-      if (parentId !== null) {
-        const parent = accumulator[parentId]
-        if (!Array.isArray(accumulator[parentId].children)) parent.children = []
-        parent.children.push(businessCapability)
-        delete accumulator[businessCapability.id]
-      }
-      return accumulator
-    }, businessCapabilityIndex)
-  const bcMaps = Object.values(businessCapabilityMapIndex)
+  const childrenFragment = '...on BusinessCapability { children:relToChild { edges { node { id factSheet { id type name description level {{children}} } } } } }'
+  query = [...Array(maxHierarchyLevel).keys()]
+    .reduce((accumulator, _, level) => accumulator.replace('{{children}}', level < (maxHierarchyLevel - 1) ? childrenFragment : ''), query)
+
+  const unrollChildren = node => {
+    let { id, description = null, factSheet = null, children: { edges: children = null } = {} } = node
+    if (children === null && factSheet !== null) ({ description = null, children: { edges: children = [] } = {} } = factSheet)
+    if (description === null) description = '{}'
+    try {
+      description = JSON.parse(description === null ? '{}' : description)
+    } catch (error) {
+      console.error(error)
+      description = {}
+    }
+    children = children.map(({ node }) => unrollChildren(node))
+    node = { ...(factSheet === null ? node : factSheet), relToParentId: factSheet === null ? null : id, children }
+    delete node.description
+    return { ...node, ...description }
+  }
+
+  const bcMaps = await graphql.executeGraphQL(query)
+    .then(({ allFactSheets: { edges } }) => edges.map(({ node }) => unrollChildren(node)))
+
   const { workspaceId, instance } = authenticator
   return { workspaceId, instance, timestamp: new Date().toISOString(), bcMaps }
 }
